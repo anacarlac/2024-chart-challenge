9a1b4a4cfc0e422d53136c3dff94cbfba2f51190
Ana Crispim <anacarlacrispim@gmail.com>
2024-04-06 20:03:55 -0300

    update-script-day-6

--- scripts/6.comparisons-oecd.R
+++ scripts/6.comparisons-oecd.R
@@ -100,58 +100,56 @@ graph_govspend + geom_text_repel(aes(label = paste0(round(gov_spending,1), "%"))
 ggsave("outputs/plot-day6-comparisons-oecd-using-r.png", width = 9, height = 5)
        
 
+# Separate graphs
 
-# Test 
 # Build grouped bar chart
-data_govspend %>% 
+graph_govspend <- data_govspend %>% 
   dplyr::filter(year != "2008") %>% 
   dplyr::filter(year != "2009") %>% 
   dplyr::filter(year != "2010") %>% 
+  dplyr::filter(edu_level == "Primary to post-secondary non-tertiary education") %>% 
   ggplot(aes(y=gov_spending, x=year, color=coutries)) + 
-  geom_line() +
-  geom_point() +
-  geom_line(size = 1, alpha = 0) +
-  geom_line(size = 3, alpha = 0.1) +
-  geom_line(size = 2, alpha = 0.2) +
-  geom_line(size = 1, alpha = 0.5) +
-  geom_point(size = 1, alpha = 0) +
-  geom_point(size = 3, alpha = 0.1) +
-  geom_point(size = 2, alpha = 0.2) +
-  geom_point(size = 1, alpha = 0.5) +
-  scale_color_manual(values = c("#1EECFF", "#FF9E49")) +
-  facet_grid(~edu_level) +
-  scale_x_continuous(limits = c(2011, 2020), breaks = seq(2011, 2020, by = 1)) +
-  scale_y_continuous(limits = c(0, 30), breaks = seq(0, 30, by = 10)) +
+  geom_line(size = 1, alpha = 0.8) +
+  # geom_line(size = 3, alpha = 0.05) +
+  # geom_line(size = 2, alpha = 0.1) +
+  # geom_line(size = 1, alpha = 0.8) +
+  geom_point(size = 2, alpha = 0.8) +
+  # geom_point(size = 3, alpha = 0.05) +
+  # geom_point(size = 2, alpha = 0.1) +
+  # geom_point(size = 1, alpha = 0.8) +
+  scale_color_manual(values = c("#F0803C", "#82C0CC")) + #F08800 #3D5A80
+  #facet_grid(~edu_level) +
+  scale_x_continuous(limits = c(2011, 2020.5), breaks = seq(2011, 2020.5, by = 1)) +
+  scale_y_continuous(limits = c(0, 15), breaks = seq(0, 15, by = 5)) +
   labs(
-    title = "How much are we investing in education?",
+    title = "How much are we investing in education? \nResults for primary to post-secondary non-tertiary education",
     subtitle = "Total public expenditure on education as a percentage of total government expenditure in Brazil and OECD countries",
-    caption = "Visualization by Ana Crispim. \nSource: OECD (2024), Education at a glance: Educational finance indicators, OECD Education Statistics (database), https://doi.org/10.1787/c4e1b551-en (accessed on 06 April 2024).",
+    caption = "Visualization by Ana Crispim. \nSource: OECD (2024), Education at a glance: Educational finance indicators, OECD Education Statistics (database),\nhttps://doi.org/10.1787/c4e1b551-en (accessed on 06 April 2024).",
     x = " ", y = " ",
     color = " "
   ) +
   theme(
     legend.position = "bottom",
-    legend.title = element_text(size = 10),
-    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 8),
+    legend.text = element_text(size = 8),
     legend.box.spacing = margin(1),
     legend.key.width = unit(0.5, 'cm'),
     legend.key.height = unit(0.5, 'cm'),
     panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(), 
-    plot.background = element_rect(fill = 'white'), #change plot background color
-    panel.background = element_rect(fill = 'white'), # change the panel background color (around title)
-    strip.background = element_rect(fill='white'), # change strips background color
-    #legend.background = element_rect(fill = 'white'), #change legend box background color
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = 'white', color = "white"),
+    strip.background = element_rect(colour="white", 
+                                    fill="white",),
     plot.margin = margin(t = 1, r = 2, b = 0.5, l = 0, "cm"),
     plot.title = element_text(
-      size = 14,
+      size = 12,
       face = "bold"),
     plot.subtitle = element_markdown(
-      size = 8,
+      size = 7,
       face = "bold.italic",
-      margin=margin(0,0,15,0)
+      margin=margin(0,0,10,0)
     ),
-    plot.caption = element_text(size = 6,
+    plot.caption = element_text(size = 5,
                                 color = "black", 
                                 hjust = 0,
                                 vjust = 0.5),
@@ -161,7 +159,18 @@ data_govspend %>%
     
   )
 
+
+# Add ggrepel geom to position values around geom_line and geom_point
+graph_govspend + geom_text_repel(aes(label = paste0(round(gov_spending,1), "%")), 
+                                 size = 2.5,
+                                 box.padding = 0.45, 
+                                 direction = "y",
+                                 max.overlaps = Inf, 
+                                 #min.segment.length= 0.3,
+                                 #nudge.y = 0,
+                                 force=1
+                                 )
 # Save plot
-ggsave("outputs/plot-day6-comparisons-oecd-using-r.png", width = 9, height = 5)
+ggsave("outputs/plot-day6-comparisons-oecd-primary-using-r.png", width = 6, height = 4)
 
 
